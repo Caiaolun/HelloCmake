0b77cbc51d0bccccb30fbb6ed2d7c3bd4a196b70
Caiaolun <15920101584@163.com>
2019-02-20 16:31:07 +0800

    First time Cmake

---
+++ lib/hello.h
@@ -0,0 +1,6 @@
+#ifndef _MY_HELLO__
+#define _MY_HELLO__
+
+void hello(const char* name);
+
+#endif
---
+++ src/main.c
@@ -0,0 +1,10 @@
+#include "hello.h"
+#include <stdio.h>
+int main()
+{
+	hello("Ollen");
+
+
+	getchar();
+	return 0;
+}
